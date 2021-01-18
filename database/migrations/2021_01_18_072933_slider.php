baa7d7f81e04ad5804cbab52168d8e9ace5f781b
Vladimir <vladimir@rupor.agency>
2021-01-18 11:00:31 +0300

    Создание миграции для слайдера

---
+++ database/migrations/2021_01_18_072933_slider.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Slider extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slider', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('activeFrom');
+            $table->date('activeTo');
+            $table->string('img-static');
+            $table->string('img-static-alt');
+            $table->string('img-hover');
+            $table->string('img-hover-alt');
+            $table->string('sale');
+            $table->string('get');
+            $table->string('discount');
+            $table->string('desc');
+            $table->string('coupone');
+            $table->string('time');
+            $table->text('link');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
